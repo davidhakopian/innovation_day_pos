fa6f73b6a469a2009aa5fdaeda3fce0ac4bab4c5
François-Guy Gallant <francois-guy.gallant@amilia.com>
2018-08-16 11:29:44 -0400

    Got a word list on the right, followed this tutorial up to part 5 https://flutter.io/get-started/codelab/

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(new MyApp());
 
@@ -60,20 +61,22 @@ class _MyHomePageState extends State<MyHomePage> {
         body: new Row(
           children: <Widget>[
             Expanded(
-                flex: 4,
+                flex: 3,
                 child: Column(
                   children: <Widget>[
-                    Expanded(
+                    Expanded( // container 1: top-left
                         flex: 6,
-                        child: Container(
+                        child: new Container(
                           margin: const EdgeInsets.all(10.0),
                           color: const Color(0xFF00FF00),
+                          //child: new Text('Hello World 1'),
                         )),
-                    Expanded(
+                    Expanded( // container 2: bottom-left
                         flex: 1,
-                        child: Container(
+                        child: new Container(
                           margin: const EdgeInsets.all(10.0),
                           color: const Color(0xFF00FF00),
+                          //child: new Text('Hello World 2'),
                         ))
                   ],
                 )),
@@ -81,11 +84,12 @@ class _MyHomePageState extends State<MyHomePage> {
                 flex: 1,
                 child: Column(
                   children: <Widget>[
-                    Expanded(
+                    Expanded( // container 3: right
                         flex: 6,
-                        child: Container(
+                        child: new Container(
                           margin: const EdgeInsets.all(10.0),
                           color: const Color(0xFF00FF00),
+                          child: RandomWords(),
                         ))
                   ],
                 )),
@@ -93,3 +97,83 @@ class _MyHomePageState extends State<MyHomePage> {
         ));
   }
 }
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// FGG's stuff
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+class RandomWordsState extends State<RandomWords> {
+  @override
+
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+  final Set<WordPair> _saved = new Set<WordPair>();
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        // The itemBuilder callback is called once per suggested word pairing,
+        // and places each suggestion into a ListTile row.
+        // For even rows, the function adds a ListTile row for the word pairing.
+        // For odd rows, the function adds a Divider widget to visually
+        // separate the entries. Note that the divider may be difficult
+        // to see on smaller devices.
+        itemBuilder: (context, i) {
+          // Add a one-pixel-high divider widget before each row in theListView.
+          if (i.isOdd) return Divider();
+
+          // The syntax "i ~/ 2" divides i by 2 and returns an integer result.
+          // For example: 1, 2, 3, 4, 5 becomes 0, 1, 1, 2, 2.
+          // This calculates the actual number of word pairings in the ListView,
+          // minus the divider widgets.
+          final index = i ~/ 2;
+          // If you've reached the end of the available word pairings...
+          if (index >= _suggestions.length) {
+            // ...then generate 10 more and add them to the suggestions list.
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        }
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: new Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
+    );
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold (
+      appBar: AppBar(
+        title: Text('Invoice info'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => new RandomWordsState();
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// end of FGG's stuff
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
